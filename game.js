6d528c12b9c9ba8d8aff251071142feffbbcf1cf
Amy Roche <amy.roche31@gmail.com>
2017-04-21 12:10:03 -0600

    Completed step 6

--- game.js
+++ game.js
@@ -1,23 +1,29 @@
 var health = 100;
+var name = document.getElementById("name").innerText = "Alien";
+var hits = 0;
 var count;
 
 function slap() {
     //window.alert(health);
     count = health - 1;
+    hits++;
     update();
 }
 
 function punch() {
     count = health - 5;
+    hits++;
     update();
 }
 
 function kick() {
     count = health - 10;
+    hits++;
     update();
 }
 
 function update() {
     document.getElementById("health").innerText = count;
     health = count;
+    document.getElementById("hits").innerText = hits;
 }
