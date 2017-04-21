82e0f5cc60b18a44130b4d548dfb67d106764099
Amy Roche <amy.roche31@gmail.com>
2017-04-21 12:05:24 -0600

    Added punch and kick

--- game.js
+++ game.js
@@ -3,12 +3,21 @@ var count;
 
 function slap() {
     //window.alert(health);
-    count = health--;
+    count = health - 1;
+    update();
+}
+
+function punch() {
+    count = health - 5;
+    update();
+}
+
+function kick() {
+    count = health - 10;
     update();
 }
 
 function update() {
     document.getElementById("health").innerText = count;
+    health = count;
 }
-
-slap();
