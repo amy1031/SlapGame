f0d21be080c246afe9a21f6a950b74d5438841da
Amy Roche <amy.roche31@gmail.com>
2017-04-21 14:38:12 -0600

    constructors

--- game.js
+++ game.js
@@ -1,32 +1,49 @@
-var health = 100;
-var name = document.getElementById("name").innerText = "Alien";
-var hits = 0;
+//var health = 100;
+//var name = document.getElementById("name").innerText = "Alien";
+//var hits = 0;
 
-function slap() {
-    //window.alert(health);
-    health = health - 1;
-    hits++;
-    update();
-}
-
-function punch() {
-    health = health - 5;
-    hits++;
-    update();
-}
-
-function kick() {
-    health = health - 10;
-    hits++;
-    update();
-}
-
-function update() {
-    document.getElementById("health").innerText = health;
-    health = health;
-    document.getElementById("hits").innerText = hits;
-
-    if (health <= 0) {
-        document.getElementById("health").innerText = name + " is Dead!";
+function Alien(name, slap, punch, kick){
+    this.name = name;
+    this.img = "https://robohash.org/" +name+"?set=set2";
+    this.health = 100;
+    this.hits = 0;
+    this.attacks = {
+        slap: slap,
+        punch: punch,
+        kick: kick
     }
 }
+
+var mark = new Alien("Mark", 1, 5, 10)
+var amy = new Alien("Amy", 1, 2, 5)
+
+
+function attack(targetPlayer, attackType) { 
+    //window.alert(health);
+    targetPlayer.health = targetPlayer.health - targetPlayer.attacks[attackType];
+    targetPlayer.hits++;
+    update(targetPlayer);
+}
+
+// function punch(targetPlayer) {
+//     targetPlayer.health = targetPlayer.health - 5;
+//     targetPlayer.hits++;
+//     update();
+// }
+
+// function kick(targetPlayer) {
+//     targetPlayer.health = targetPlayer.health - 10;
+//     targetPlayer.hits++;
+//     update();
+// }
+
+function update(targetPlayer) {
+    document.getElementById("health").innerText = targetPlayer.health;
+    targetPlayer.health = targetPlayer.health;
+    document.getElementById("hits").innerText = targetPlayer.hits;
+
+    if (targetPlayer.health <= 0) {
+        document.getElementById("health").innerText = targetPlayer.name + " is Dead!";
+    }
+}
+
