820de06f3da40ca13a2f7045a8a4a50aadce5eef
Amy Roche <amy.roche31@gmail.com>
2017-04-21 12:12:43 -0600

    removed unnecessary variables and tested step 7

--- game.js
+++ game.js
@@ -1,29 +1,28 @@
 var health = 100;
 var name = document.getElementById("name").innerText = "Alien";
 var hits = 0;
-var count;
 
 function slap() {
     //window.alert(health);
-    count = health - 1;
+    health = health - 1;
     hits++;
     update();
 }
 
 function punch() {
-    count = health - 5;
+    health = health - 5;
     hits++;
     update();
 }
 
 function kick() {
-    count = health - 10;
+    health = health - 10;
     hits++;
     update();
 }
 
 function update() {
-    document.getElementById("health").innerText = count;
-    health = count;
+    document.getElementById("health").innerText = health;
+    health = health;
     document.getElementById("hits").innerText = hits;
 }
