31475d9d629d3c42240e7df1adb778b6adea653f
Amy Roche <amy.roche31@gmail.com>
2017-04-28 15:29:02 -0600

    attacks are working

--- game-controller.js
+++ game-controller.js
@@ -1,19 +1,29 @@
 function AlienController() {
     //PRIVATE
     var alienService = new AlienService();
-    
+
     var strength = ["Strong", "Medium", "Weak"];
     var randomStrength = strength[Math.floor(Math.random() * strength.length)];
 
 
     //PUBLIC
     this.update = function update(targetPlayer) {
-        document.getElementById("health").innerText = targetPlayer.health;
+        //var targetAlien = alienService.getAlien(targetPlayer);
+        document.getElementById("health").innerText = `Health: ${targetPlayer.health}`;
         targetPlayer.health = targetPlayer.health;
         document.getElementById("hits").innerText = targetPlayer.hits;
 
         if (targetPlayer.health <= 0) {
          document.getElementById("health").innerText = targetPlayer.name + " coffee is ready! Coffee strength is " + randomStrength;
         }
+    }
+
+    this.attack = function attack(targetPlayer, attackType) { 
+        //window.alert(health);
+        var targetAlien = alienService.getAlien(targetPlayer);
+        var mod = alienService.calcItems(targetAlien)
+        targetAlien.health = targetAlien.health - (targetAlien.attacks[attackType] + mod);
+        targetAlien.hits++;
+        this.update(targetAlien);
     }
 }
--- game-service.js
+++ game-service.js
@@ -38,31 +38,24 @@ function AlienService() {
     modifiers.grind = new Item("Grind", 4, "Turn the alien course!");
     modifiers.sugar = new Item("Sugar", 10, "Aliens hate sugar!");
 
-    function giveItem(targetPlayer, type) {
-        targetPlayer.items[type] = modifiers[type]
-        // console.log(targetPlayer.items)
-    }
 
 
     // PUBLIC
-    this.attack = function attack(targetPlayer, attackType) { 
-        //window.alert(health);
-        var mod = calcItems(targetPlayer)
-        targetPlayer.health = targetPlayer.health - (targetPlayer.attacks[attackType]+mod);
-        targetPlayer.hits++;
-        update(targetPlayer);
-    }
-
     this.getAlien = function(targetName) {
         return getAlien(targetName)
     }
 
     this.calcItems = function calcItems(targetPlayer) {
-    var total = 0;
-    for(var mod in targetPlayer.items) {
-        total += targetPlayer.items[mod].value;
-    } 
-    console.log(total)
-    return total;
-}
+        var total = 0;
+        for(var mod in targetPlayer.items) {
+            total += targetPlayer.items[mod].value;
+            } 
+       // console.log(total)
+        return total;
+    }
+
+    this.giveItem = function giveItem(targetPlayer, type) {
+        targetPlayer.items[type] = modifiers[type]
+        // console.log(targetPlayer.items)
+    }
 }
